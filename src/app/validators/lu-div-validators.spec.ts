4a37a4be1ac048eb33137139895452f8605a8075
GABA-MANO <ybgabamano@gmail.com>
2022-03-30 15:17:40 +0200

    Mise en place du formulaire complet

---
+++ src/app/validators/lu-div-validators.spec.ts
@@ -0,0 +1,7 @@
+import { LuDivValidators } from './lu-div-validators';
+
+describe('LuDivValidators', () => {
+  it('should create an instance', () => {
+    expect(new LuDivValidators()).toBeTruthy();
+  });
+});
